1a8867b9f69a0595b353fef83305ed20382ad1e0
Dan <daniiomir@yandex.ru>
2021-03-01 20:23:18 +0300

    Created preprocess script (it splits data to train and test) and make some EDA.

---
+++ preprocess.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from src.tools import LabelEncoder, parse_args_preprocess, most_correlated_features, make_corr_heatmap, save
+
+if __name__ == '__main__':
+    args = parse_args_preprocess()
+    dataset = pd.read_csv(args['dataset_path'])
+    encoder = LabelEncoder()
+
+    dataset = pd.DataFrame(data=dataset.to_numpy(), columns=[str(i + 1) for i in range(dataset.shape[1])])
+    labels = encoder.fit_transform(dataset['2'])
+    dataset.drop(columns=['2'], inplace=True, axis=1)
+    dataset = dataset.astype(np.float64)
+    dataset['labels'] = labels
+
+    drop_features = most_correlated_features(dataset.drop(['labels'], axis=1), 0.9)
+
+    print(dataset.describe().to_string())
+    make_corr_heatmap(dataset)
+    print('\nMost correlated features (over 0.9) - ', drop_features)
+
+    print('\nBefore removing trash - ', dataset.shape)
+    dataset.drop(columns=drop_features, axis=1, inplace=True)
+    dataset = dataset.dropna()
+    print('After removing trash - ', dataset.shape)
+
+    y = dataset['labels']
+    x = dataset.drop(columns=['labels'], axis=1)
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=args['seed'])
+
+    save((X_train, y_train), 'tmp/train.pkl')
+    save((X_test, y_test), 'tmp/test.pkl')
+    save(encoder, 'tmp/encoder.pkl')
+
+    print('\nDataset prepossessing finished.')
--- src/tools.py
+++ src/tools.py
@@ -1,5 +1,9 @@
+import pickle
+import argparse
 import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 class StandartScaler:
@@ -31,6 +35,88 @@ class StandartScaler:
                 data[col] = data[col].apply(self._scale, mean=self.mean[col], std=self.std[col])
             return data
         raise Exception('Dataframe columns are not equal to previous.')
+
+
+class LabelEncoder:
+    def __init__(self):
+        self.mapping = None
+
+    def _fit(self, targets: np.ndarray):
+        classes = np.unique(targets)
+        self.mapping = {classes[i]: i for i in range(len(classes))}
+
+    def fit_transform(self, targets: np.ndarray):
+        self._fit(targets)
+        return self.transform(targets)
+
+    def transform(self, targets: np.ndarray):
+        if self.mapping is not None:
+            encoded = np.zeros(targets.shape, dtype=int)
+            for k, v in self.mapping.items():
+                encoded[targets == k] = v
+            return encoded
+        raise Exception('You should do fit_transform first!')
+
+    def reverse_transform(self, reversed_targets: np.ndarray):
+        if self.mapping is not None:
+            targets = np.zeros(reversed_targets.shape, dtype=object)
+            for k, v in self.mapping.items():
+                targets[reversed_targets == v] = k
+            return targets
+        raise Exception('You should do fit_transform first!')
+
+
+def most_correlated_features(df, threshold):
+    cor_matrix = df.corr().abs()
+    upper_tri = cor_matrix.where(np.triu(np.ones(cor_matrix.shape), k=1).astype(np.bool))
+    return [column for column in upper_tri.columns if any(upper_tri[column] > threshold)]
+
+
+def make_corr_heatmap(dataset):
+    corr = dataset.corr()
+    plt.figure(figsize=(20, 18))
+    sns.heatmap(corr,
+                xticklabels=corr.columns,
+                yticklabels=corr.columns, annot=True)
+    plt.tight_layout()
+    plt.savefig('imgs/corr_plot.png')
+    plt.close()
+
+
+def parse_args_preprocess():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset_path', default='data/data.csv')
+    parser.add_argument('--seed', default=42)
+    args = parser.parse_args()
+    return args.__dict__
+
+
+def parse_args_train():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train_df_path', default='tmp/train.pkl')
+    parser.add_argument('--save_model_path', default='tmp/model.pkl')
+    parser.add_argument('--seed', default=42)
+    args = parser.parse_args()
+    return args.__dict__
+
+
+def parse_args_test():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--test_df_path', default='tmp/test.pkl')
+    parser.add_argument('--load_model_path', default='tmp/model.pkl')
+    args = parser.parse_args()
+    return args.__dict__
+
+
+def save(obj: object, path: str):
+    with open(path, 'wb') as f:
+        pickle.dump(obj, f, protocol=4)
+
+
+def load(path: str):
+    with open(path, 'rb') as f:
+        obj = pickle.load(f)
+    return obj
 
 
 if __name__ == '__main__':
