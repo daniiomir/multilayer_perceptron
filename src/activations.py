Fixed bug with NaNs. Gradient clipping was added to optimizers. Added funcs to load and save weights. Added func which create learning curves. Fixed softmax computation.

src/activations.py
@@ -12,7 +12,7 @@ class ReLU(Layer):
 
     def backward(self, input: np.ndarray, grad_output: np.ndarray) -> np.ndarray:
         relu_grad = input > 0
-        return grad_output * relu_grad
+        return grad_output * relu_grad.astype(int)
 
 
 class Tanh(Layer):
@@ -43,8 +43,10 @@ class SoftMax(Layer):
         super().__init__()
 
     def forward(self, input: np.ndarray) -> np.ndarray:
-        e_x = np.exp(input - np.max(input))
-        return e_x / e_x.sum(axis=0)
+        assert len(input.shape) == 2
+        e_x = np.exp(input - np.max(input, axis=1)[:, np.newaxis])
+        div = np.sum(e_x, axis=1)[:, np.newaxis]
+        return e_x / div
 
     def backward(self, input: np.ndarray, grad_output: np.ndarray) -> np.ndarray:
         p = self.forward(input)

src/optimizers.py
@@ -11,23 +11,25 @@ class Optimizer:
 
 
 class SGD(Optimizer):
-    def __init__(self, params, learning_rate):
+    def __init__(self, params, learning_rate, grad_clip=5):
         super().__init__(params, learning_rate)
         self.model_params = params
         self.learning_rate = learning_rate
+        self.grad_clip = grad_clip
 
     def step(self):
         for layer in self.model_params:
-            layer.weights -= self.learning_rate * layer.weights_grad
-            layer.biases -= self.learning_rate * layer.biases_grad
+            layer.weights -= self.learning_rate * np.clip(layer.weights_grad, -self.grad_clip, self.grad_clip)
+            layer.biases -= self.learning_rate * np.clip(layer.biases_grad, -self.grad_clip, self.grad_clip)
 
 
 class Momentum(Optimizer):
-    def __init__(self, params, learning_rate, momentum=0.99):
+    def __init__(self, params, learning_rate, momentum=0.99, grad_clip=5):
         super().__init__(params, learning_rate)
         self.model_params = params
         self.learning_rate = learning_rate
         self.momentum = momentum
+        self.grad_clip = grad_clip
         self.weights_velocities = {i: np.zeros_like(self.model_params[i].weights) for i in
                                    range(len(self.model_params))}
         self.biases_velocities = {i: np.zeros_like(self.model_params[i].biases) for i in
@@ -36,9 +38,9 @@ class Momentum(Optimizer):
     def step(self):
         for index, layer in enumerate(self.model_params):
             self.weights_velocities[index] = self.momentum * self.weights_velocities[index] + \
-                                             self.learning_rate * layer.weights_grad
+                                             self.learning_rate * np.clip(layer.weights_grad, -self.grad_clip, self.grad_clip)
             self.biases_velocities[index] = self.momentum * self.biases_velocities[index] + \
-                                            self.learning_rate * layer.biases_grad
+                                            self.learning_rate * np.clip(layer.biases_grad, -self.grad_clip, self.grad_clip)
             layer.weights -= self.weights_velocities[index]
             layer.biases -= self.biases_velocities[index]
 

src/tools.py
@@ -90,6 +90,17 @@ def make_count_plot(df: pd.DataFrame, df_col: pd.Series) -> None:
     plt.close()
 
 
+def make_learning_curves(train_list, val_list, name):
+    plt.plot(np.arange(len(train_list)), train_list, color='blue', label='train')
+    plt.plot(np.arange(len(val_list)), val_list, color='red', label='val')
+    plt.legend(loc='best')
+    plt.title(f'Development of {name.capitalize()} during training')
+    plt.xlabel('Number of iterations')
+    plt.ylabel(name.capitalize())
+    plt.savefig(f'imgs/{name}.png')
+    plt.close()
+
+
 def parse_args_preprocess() -> dict:
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset_path', default='data/data.csv')
@@ -103,7 +114,7 @@ def parse_args_train() -> dict:
     parser = argparse.ArgumentParser()
     parser.add_argument('--save_model_path', default='tmp/model.pkl')
     parser.add_argument('--seed', default=42)
-    parser.add_argument('--epochs', default=100)
+    parser.add_argument('--epochs', default=30)
     parser.add_argument('--batchsize', default=32)
     args = parser.parse_args()
     return args.__dict__

train.py
@@ -1,5 +1,5 @@
 import numpy as np
-from src.tools import parse_args_train, load
+from src.tools import parse_args_train, load, make_learning_curves
 from src.nn_tools import seed_everything, init_weights, dataloader
 from src.layers import Dense
 from src.losses import BinaryCrossEntropyLoss, CrossEntropyLoss
@@ -10,9 +10,6 @@ from src.model import Model
 if __name__ == '__main__':
     args = parse_args_train()
     seed_everything(args['seed'])
-    train_log = []
-    val_log = []
-    loss_by_epoch = []
 
     X_train, y_train = load('tmp/train.pkl')
     X_val, y_val = load('tmp/val.pkl')
@@ -30,33 +27,54 @@ if __name__ == '__main__':
     model.add_layer(Dense(200, 50))
     model.add_layer(ReLU())
     model.add_layer(Dense(50, 2))
-    model.add_layer(SoftMax())
+    # model.add_layer(SoftMax())
 
     init_weights(model.params, 'kaiming_normal')
 
     criterion = CrossEntropyLoss()
-    optimizer = Momentum(model.params, 0.1)
+    optimizer = Momentum(model.params, 1e-4, grad_clip=3)
+
+    train_acc_by_epoch = []
+    val_acc_by_epoch = []
+
+    train_loss_by_epoch = []
+    val_loss_by_epoch = []
 
     for epoch in range(args['epochs']):
-        loss_by_batch = []
+        train_loss_by_batch = []
+        val_loss_by_batch = []
+
         for x_batch, y_batch in dataloader(X_train.to_numpy(), y_train.to_numpy(), batchsize=args['batchsize'],
                                            shuffle=True):
             preds = model.forward(x_batch, mode='train')
             loss = criterion(y_batch, preds)
-            loss_by_batch.append(loss)
+            train_loss_by_batch.append(loss)
             criterion.backward(model, y_batch, preds)
             optimizer.step()
             model.clear_cache()
 
+        for x_batch_val, y_batch_val in dataloader(X_val.to_numpy(), y_val.to_numpy(), batchsize=args['batchsize'],
+                                           shuffle=True):
+            preds = model.forward(x_batch_val)
+            loss = criterion(y_batch_val, preds)
+            val_loss_by_batch.append(loss)
+
+        train_loss_by_epoch.append(np.mean(train_loss_by_batch))
+        val_loss_by_epoch.append(np.mean(val_loss_by_batch))
+
         train_acc = np.mean(np.argmax(model.forward(X_train), axis=1) == y_train)
         val_acc = np.mean(np.argmax(model.forward(X_val), axis=1) == y_val)
 
-        print(f'[{epoch + 1} epoch] loss - {round(np.mean(loss_by_batch), 5)}, '
+        train_acc_by_epoch.append(train_acc)
+        val_acc_by_epoch.append(val_acc)
+
+        print(f'[{epoch + 1}/{args["epochs"]} epoch] '
+              f'train loss - {round(np.mean(train_loss_by_batch), 5)}, '
+              f'val loss - {round(np.mean(val_loss_by_batch), 5)}, '
               f'train acc - {round(train_acc, 5)}, '
               f'val acc - {round(val_acc, 5)}')
 
-        train_log.append(train_acc)
-        val_log.append(val_acc)
-        loss_by_epoch.append(np.mean(loss_by_batch))
+    make_learning_curves(train_loss_by_epoch, val_loss_by_epoch, name='loss')
+    make_learning_curves(train_acc_by_epoch, val_acc_by_epoch, name='accuracy')
 
     print('Training finished.')