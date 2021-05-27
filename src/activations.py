Fixed SoftMax, CrossEntropyLoss. Added script of multiclass classification on fisher's iris dataset.

src/activations.py
@@ -43,9 +43,10 @@ class SoftMax(Layer):
         super().__init__()
 
     def forward(self, input: np.ndarray, mode: str = 'train') -> np.ndarray:
-        e_x = np.exp(input - np.max(input, axis=1)[:, np.newaxis])
-        div = np.sum(e_x, axis=1)[:, np.newaxis]
-        return e_x / div
+        exp = np.exp(input - np.max(input))
+        for i in range(len(input)):
+            exp[i] /= np.sum(exp[i])
+        return exp
 
     def backward(self, input: np.ndarray, grad_output: np.ndarray) -> np.ndarray:
         p = self.forward(input)

src/losses.py
@@ -14,6 +14,12 @@ class Loss:
 
 
 class BinaryCrossEntropyLoss(Loss):
+    """
+    Loss for binary classification
+
+    y_pred: [0.5, ..., 0.1] | shape - (probabilities,)
+    y_true: [0.5, ..., 0.1] | shape - (probabilities,)
+    """
     def __init__(self):
         super().__init__()
 
@@ -29,20 +35,23 @@ class BinaryCrossEntropyLoss(Loss):
 
 
 class CrossEntropyLoss(Loss):
+    """
+    Loss for multiclass classification
+
+    y_true: [[0, 0, 1], [...], [...]] | one hot encoded labels | shape - (batch_size, num_classes)
+    y_pred: [[0.1, 0.3, 0.6], [...], [...]] | shape - (batch_size, num_classes)
+    """
     def __init__(self):
         super().__init__()
 
     def __call__(self, y_true: np.ndarray, y_pred: np.ndarray):
-        target = y_true[:, np.newaxis]
-        if target.shape[1] == 1:
-            target = np.append(1 - target, target, axis=1)
         output = np.clip(y_pred, self.eps, 1. - self.eps)
         output /= output.sum(axis=1)[:, np.newaxis]
-        loss = -(target * np.log(output)).sum(axis=1)
+        loss = -(y_true * np.log(output)).sum(axis=1)
         return np.mean(loss)
 
     def backward(self, model: Model, y_true: np.ndarray, y_pred: np.ndarray):
-        loss_grad = y_pred - y_true[:, np.newaxis]
+        loss_grad = y_pred - y_true
         model.backward(model.forward_list, loss_grad)
 
 

tests/test_iris.py
@@ -1,30 +1,28 @@
 import argparse
 import numpy as np
-import pandas as pd
 
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier
 
-from src.plots import make_learning_curves
-from src.tools import load, save, LabelEncoder, StandartScaler, most_correlated_features
-from src.nn_tools import seed_everything, init_weights, dataloader, check_best_model, threshold_prediction, split_to_train_val_test
-from src.layers import Dense
-from src.losses import BinaryCrossEntropyLoss
-from src.activations import ReLU, Sigmoid
-from src.optimizers import SGD, Momentum, RMSProp, Adam
 from src.model import Model
+from src.layers import Dense
+from src.activations import ReLU, SoftMax
+from src.losses import CrossEntropyLoss
+from src.optimizers import SGD, Momentum, RMSProp, Adam
+from src.nn_tools import split_to_train_val_test, seed_everything, init_weights, threshold_prediction, dataloader, \
+    check_best_model, clip_gradients
+from src.plots import make_learning_curves
 
 
 def parse_args() -> dict:
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', default=42)
-    parser.add_argument('--preprocess', default='no')
-    parser.add_argument('--learning_rate', default=0.01)
-    parser.add_argument('--epochs', default=50)
+    parser.add_argument('--learning_rate', default=0.00001)
+    parser.add_argument('--epochs', default=1000)
     parser.add_argument('--esr', default=10)
-    parser.add_argument('--clip_grad', default=3.)
     parser.add_argument('--batchsize', default=32)
-    parser.add_argument('--dataset_path', default='../data/breast_cancer_dataset.csv')
     parser.add_argument('--test_size', default=0.33)
     parser.add_argument('--save_weights_path', default='../tmp/weights.pkl')
     parser.add_argument('--train_data_path', default='../tmp/train.pkl')
@@ -37,68 +35,36 @@ def parse_args() -> dict:
 if __name__ == '__main__':
     args = parse_args()
     seed_everything(args['seed'])
+    encoder = OneHotEncoder(sparse=False)
+    x, y = load_iris(return_X_y=True)
 
-    if args['preprocess'] == 'yes':
-        dataset = pd.read_csv(args['dataset_path'])
-        encoder = LabelEncoder()
-        scaler = StandartScaler()
+    X_train, y_train, X_val, y_val, X_test, y_test = split_to_train_val_test(x, y, args['test_size'], args['seed'])
 
-        dataset = pd.DataFrame(data=dataset.to_numpy(), columns=[str(i + 1) for i in range(dataset.shape[1])])
-
-        labels = encoder.fit_transform(dataset['2'])
-        dataset.drop(columns=['1', '2'], inplace=True, axis=1)
-        dataset = dataset.astype(np.float64)
-        dataset['labels'] = labels
-
-        drop_features = most_correlated_features(dataset.drop(['labels'], axis=1), 0.9)
-
-        print(dataset.describe().to_string())
-        print('\nMost correlated features (over 0.9) - ', drop_features)
-        print('\nBefore removing trash - ', dataset.shape)
-        dataset.drop(columns=drop_features, axis=1, inplace=True)
-        dataset = dataset.dropna()
-        print('After removing trash - ', dataset.shape)
-
-        y = dataset['labels']
-        x = dataset.drop(columns=['labels'], axis=1)
-
-        x = scaler.fit_transform(x)
-
-        X_train, y_train, X_val, y_val, X_test, y_test = split_to_train_val_test(x, y, args['test_size'], args['seed'])
-
-        save((X_train, y_train), args['train_data_path'])
-        save((X_val, y_val), args['val_data_path'])
-        save((X_test, y_test), args['test_data_path'])
-        save(encoder, '../tmp/encoder.pkl')
-
-        print('\nDataset prepossessing finished.')
-
-    else:
-        X_train, y_train = load(args['train_data_path'])
-        X_val, y_val = load(args['val_data_path'])
-        X_test, y_test = load(args['test_data_path'])
+    y_train = encoder.fit_transform(y_train[:, np.newaxis])
+    y_val = encoder.transform(y_val[:, np.newaxis])
+    y_test = encoder.transform(y_test[:, np.newaxis])
 
     model = Model()
-    model.add_layer(Dense(X_train.shape[1], 20))
+    model.add_layer(Dense(X_train.shape[1], 10))
     model.add_layer(ReLU())
-    model.add_layer(Dense(20, 50))
+    model.add_layer(Dense(10, 10))
     model.add_layer(ReLU())
-    model.add_layer(Dense(50, 10))
+    model.add_layer(Dense(10, 5))
     model.add_layer(ReLU())
-    model.add_layer(Dense(10, 1))
-    model.add_layer(Sigmoid())
+    model.add_layer(Dense(5, 3))
+    model.add_layer(SoftMax())
 
     init_weights(model.params, 'kaiming_normal')
 
-    criterion = BinaryCrossEntropyLoss()
-    optimizer = SGD(model.params, float(args['learning_rate']))
+    criterion = CrossEntropyLoss()
+    optimizer = Momentum(model.params, float(args['learning_rate']))
+
+    print(f'Selected params: {args}')
 
     train_acc_by_epoch = []
     val_acc_by_epoch = []
     train_loss_by_epoch = []
     val_loss_by_epoch = []
-
-    print(f'Selected params: {args}')
 
     for epoch in range(int(args['epochs'])):
         train_loss_by_batch = []
@@ -107,12 +73,12 @@ if __name__ == '__main__':
         val_acc_by_batch = []
 
         model.train_mode()
-        for x_batch_train, y_batch_train in dataloader(X_train.to_numpy(), y_train.to_numpy(),
+        for x_batch_train, y_batch_train in dataloader(X_train, y_train,
                                                        batchsize=int(args['batchsize']),
                                                        shuffle=True):
             preds = model.forward(x_batch_train)
             loss = criterion(y_batch_train, preds)
-            acc = np.mean(threshold_prediction(preds)[:, 0] == y_batch_train)
+            acc = np.mean(np.argmax(preds, axis=1) == np.argmax(y_batch_train, axis=1))
             train_loss_by_batch.append(loss)
             train_acc_by_batch.append(acc)
             criterion.backward(model, y_batch_train, preds)
@@ -120,12 +86,12 @@ if __name__ == '__main__':
             model.clear_cache()
 
         model.test_mode()
-        for x_batch_val, y_batch_val in dataloader(X_val.to_numpy(), y_val.to_numpy(),
+        for x_batch_val, y_batch_val in dataloader(X_val, y_val,
                                                    batchsize=int(args['batchsize']),
                                                    shuffle=True):
             preds = model.forward(x_batch_val)
             loss = criterion(y_batch_val, preds)
-            acc = np.mean(threshold_prediction(preds)[:, 0] == y_batch_val)
+            acc = np.mean(np.argmax(preds, axis=1) == np.argmax(y_batch_val, axis=1))
             val_loss_by_batch.append(loss)
             val_acc_by_batch.append(acc)
 
@@ -158,10 +124,10 @@ if __name__ == '__main__':
     print('=' * 20 + '\n')
 
     model.test_mode()
-    test_acc = np.mean(threshold_prediction(model.forward(X_test.to_numpy()))[:, 0] == y_test)
-    lr = LogisticRegression(random_state=args['seed']).fit(X_train, y_train)
-    mlp = MLPClassifier(random_state=args['seed'], max_iter=1000, hidden_layer_sizes=(20, 50)).fit(X_train, y_train)
+    test_acc = np.mean(np.argmax(model.forward(X_test), axis=1) == np.argmax(y_test, axis=1))
+    lr = LogisticRegression(random_state=args['seed']).fit(X_train, np.argmax(y_train, axis=1))
+    mlp = MLPClassifier(random_state=args['seed'], max_iter=1000, hidden_layer_sizes=(20, 50)).fit(X_train, np.argmax(y_train, axis=1))
 
     print('My mlp model accuracy - ', round(test_acc, 4))
-    print('Sklearn logistic regression accuracy - ', round(np.mean(lr.predict(X_test) == y_test), 4))
-    print('Sklearn mlp accuracy - ', round(np.mean(mlp.predict(X_test) == y_test), 4))
+    print('Sklearn logistic regression accuracy - ', round(np.mean(lr.predict(X_test) == np.argmax(y_test, axis=1)), 4))
+    print('Sklearn mlp accuracy - ', round(np.mean(mlp.predict(X_test) == np.argmax(y_test, axis=1)), 4))