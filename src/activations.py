Some fixes in EarlyStopping and SoftMax. Added initialization of bias in init_weights.

src/activations.py
@@ -43,9 +43,9 @@ class SoftMax(Layer):
         super().__init__()
 
     def forward(self, input: np.ndarray, mode: str = 'train') -> np.ndarray:
-        exp = np.exp(input - np.max(input))
+        exp = np.exp(input)  # - np.max(input)
         for i in range(len(input)):
-            exp[i] /= np.sum(exp[i])
+            exp[i] /= np.sum(exp[i], axis=0)
         return exp
 
     def backward(self, input: np.ndarray, grad_output: np.ndarray) -> np.ndarray:

src/nn_tools.py
@@ -18,7 +18,7 @@ class EarlyStopping:
             print(f'Current early stopping rate bigger than {self.esr}. Stop training!')
             return True
         if len(self.loss_list) > 1:
-            if max(self.loss_list[:-1]) > self.loss_list[-1]:
+            if min(self.loss_list[:-1]) < self.loss_list[-1]:
                 self.current_esr += 1
             else:
                 self.current_esr = 0
@@ -44,16 +44,25 @@ def dataloader(inputs: np.ndarray, targets: np.ndarray, batchsize: int = 32,
         yield inputs[excerpt], targets[excerpt]
 
 
-def init_weights(params: list, method: str = 'xavier_normal') -> None:
+def init_weights(params: list, weights_method: str = 'xavier_normal', biases_method: str = 'zeros') -> None:
     for layer in params:
-        if method == 'zeros':
+        if weights_method == 'zeros':
             layer.weights = np.zeros(layer.shape)
-        elif method == 'normal':
+        elif weights_method == 'normal':
             layer.weights = np.random.normal(loc=0., scale=1., size=layer.shape)
-        elif method == 'xavier_normal':
+        elif weights_method == 'xavier_normal':
             layer.weights = np.random.normal(loc=0., scale=(1. / np.sqrt(layer.shape[0])), size=layer.shape)
-        elif method == 'kaiming_normal':
+        elif weights_method == 'kaiming_normal':
             layer.weights = np.random.normal(loc=0., scale=(np.sqrt(2. / layer.shape[0])), size=layer.shape)
+        else:
+            raise NotImplementedError
+
+        if biases_method == 'zeros':
+            layer.biases = np.zeros((layer.shape[1]))
+        elif biases_method == 'ones':
+            layer.biases = np.ones((layer.shape[1]))
+        elif biases_method == 'normal':
+            layer.biases = np.random.normal(loc=0., scale=1., size=layer.shape[1])
         else:
             raise NotImplementedError
 

tests/test_mnist_fcn_mc.py
@@ -2,7 +2,7 @@ import mnist
 import argparse
 import numpy as np
 
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier
 
@@ -11,18 +11,18 @@ from src.layers import Dense
 from src.activations import ReLU, SoftMax
 from src.losses import CrossEntropyLoss
 from src.optimizers import SGD, Momentum, RMSProp, Adam
-from src.nn_tools import split_to_train_val_test, seed_everything, init_weights, threshold_prediction, dataloader, \
-    check_best_model, clip_gradients
+from src.nn_tools import split_to_train_val_test, seed_everything, init_weights, dataloader, \
+    check_best_model, clip_gradients, EarlyStopping
 from src.plots import make_learning_curves
 
 
 def parse_args() -> dict:
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', default=42)
-    parser.add_argument('--learning_rate', default=0.0001)
+    parser.add_argument('--learning_rate', default=0.00001)
     parser.add_argument('--epochs', default=300)
     parser.add_argument('--esr', default=10)
-    parser.add_argument('--batchsize', default=32)
+    parser.add_argument('--batchsize', default=64)
     parser.add_argument('--test_size', default=0.33)
     parser.add_argument('--save_weights_path', default='../tmp/weights.pkl')
     parser.add_argument('--dataset_folder', default='../data/mnist')
@@ -34,6 +34,7 @@ if __name__ == '__main__':
     args = parse_args()
     seed_everything(args['seed'])
     encoder = OneHotEncoder(sparse=False)
+    scaler = StandardScaler()
 
     mnist.temporary_dir = lambda: args['dataset_folder']
     X_train = mnist.train_images()
@@ -46,6 +47,7 @@ if __name__ == '__main__':
     y_test = mnist.test_labels()
 
     x, y = np.concatenate((X_train, X_test)), np.concatenate((y_train, y_test))
+    x = scaler.fit_transform(x)
     X_train, y_train, X_val, y_val, X_test, y_test = split_to_train_val_test(x, y, args['test_size'], args['seed'])
 
     y_train = encoder.fit_transform(y_train[:, np.newaxis])
@@ -53,19 +55,20 @@ if __name__ == '__main__':
     y_test = encoder.transform(y_test[:, np.newaxis])
 
     model = Model()
-    model.add_layer(Dense(X_train.shape[1], 20))
+    model.add_layer(Dense(X_train.shape[1], 128))
     model.add_layer(ReLU())
-    model.add_layer(Dense(20, 50))
+    model.add_layer(Dense(128, 64))
     model.add_layer(ReLU())
-    model.add_layer(Dense(50, 10))
+    model.add_layer(Dense(64, 32))
     model.add_layer(ReLU())
-    model.add_layer(Dense(10, 10))
+    model.add_layer(Dense(32, 10))
     model.add_layer(SoftMax())
 
-    init_weights(model.params, 'kaiming_normal')
+    init_weights(model.params, 'kaiming_normal', 'zeros')
 
     criterion = CrossEntropyLoss()
     optimizer = Adam(model.params, float(args['learning_rate']))
+    es = EarlyStopping(esr=30)
 
     print(f'Selected params: {args}')
 
@@ -108,7 +111,8 @@ if __name__ == '__main__':
               f'train loss - {round(np.mean(train_loss_by_batch), 5)}, '
               f'val loss - {round(np.mean(val_loss_by_batch), 5)}, '
               f'train acc - {round(np.mean(train_acc_by_batch), 5)}, '
-              f'val acc - {round(np.mean(val_acc_by_batch), 5)}',
+              f'val acc - {round(np.mean(val_acc_by_batch), 5)}'
+              f' | current esr - {es.current_esr}',
               end='')
 
         if check_best_model(val_loss_by_epoch, np.mean(val_loss_by_batch)):
@@ -116,6 +120,10 @@ if __name__ == '__main__':
             print(' | Best model! Saving weights!', end='')
 
         print('')
+
+        es.add_loss(np.mean(val_loss_by_batch))
+        if es.check_stop_training():
+            break
 
         train_loss_by_epoch.append(np.mean(train_loss_by_batch))
         val_loss_by_epoch.append(np.mean(val_loss_by_batch))
@@ -132,11 +140,14 @@ if __name__ == '__main__':
     print('Getting metrics on test data.')
     print('=' * 20 + '\n')
 
+    model.load_weights(args['save_weights_path'])
     model.test_mode()
     test_acc = np.mean(np.argmax(model.forward(X_test), axis=1) == np.argmax(y_test, axis=1))
-    lr = LogisticRegression(random_state=args['seed'], solver='sag', max_iter=10000).fit(X_train, np.argmax(y_train, axis=1))
-    mlp = MLPClassifier(random_state=args['seed'], max_iter=10000, hidden_layer_sizes=(20, 50)).fit(X_train, np.argmax(y_train, axis=1))
 
     print('My mlp model accuracy - ', round(test_acc, 4))
+
+    lr = LogisticRegression(random_state=args['seed'], solver='sag', max_iter=1000).fit(X_train, np.argmax(y_train, axis=1))
+    mlp = MLPClassifier(random_state=args['seed'], max_iter=1000, hidden_layer_sizes=(128, 64)).fit(X_train, np.argmax(y_train, axis=1))
+
     print('Sklearn logistic regression accuracy - ', round(np.mean(lr.predict(X_test) == np.argmax(y_test, axis=1)), 4))
     print('Sklearn mlp accuracy - ', round(np.mean(mlp.predict(X_test) == np.argmax(y_test, axis=1)), 4))